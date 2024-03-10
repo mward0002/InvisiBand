97c54159965b2c685e955a3d9de9e2c20ba3e739
joshlinero <155924506+joshlinero@users.noreply.github.com>
2024-03-10 16:51:09 -0500

    Update main.c

--- software/apps/final_design/main.c
+++ software/apps/final_design/main.c
@@ -15,12 +15,15 @@
 #include "microbit_v2.h"
 
 
-#define TRIG1 EDGE_P0
-#define ECHO1 EDGE_P1
+#define TRIG1 EDGE_P7
+#define ECHO1 EDGE_P8
 #define TRIG2 EDGE_P5
 #define ECHO2 EDGE_P6
 #define TRIG3 EDGE_P11
 #define ECHO3 EDGE_P12
+#define TRIG4 EDGE_P14
+#define ECHO4 EDGE_P15
+
 
 // Global variables
 // APP_TIMER_DEF(sample_timer);
@@ -31,12 +34,20 @@ static nrfx_timer_t TIMER4 = NRFX_TIMER_INSTANCE(0);
 // Function prototypes
 static void gpio_init_main(void);
 
+
+
+
+
 static void gpio_init_main(void) {
   // Initialize pins
   gpio_edge_init(TRIG1, ECHO1);
   gpio_edge_init(TRIG2, ECHO2);
   gpio_edge_init(TRIG3, ECHO3);
 }
+
+
+
+
 
 int main(void) {
   printf("Board started!\n");
@@ -51,43 +62,49 @@ int main(void) {
     // Don't put any code in here. Instead put periodic code in `sample_timer_callback()`
     //printf("in while loop \n");
     uint32_t distance1 = hr_sc04_measure_dist(TRIG1, ECHO1, NRF_TIMER_CC_CHANNEL0);
-    /*
     uint32_t distance2 = hr_sc04_measure_dist(TRIG2, ECHO2, NRF_TIMER_CC_CHANNEL1);
     uint32_t distance3 = hr_sc04_measure_dist(TRIG3, ECHO3, NRF_TIMER_CC_CHANNEL2);
-    if ((distance1 < 10) && (distance2 >= 10) && (distance3 >= 10)){
+    uint32_t distance4 = hr_sc04_measure_dist(TRIG4, ECHO4, NRF_TIMER_CC_CHANNEL3);
+    if ((distance1 < 10) && (distance2 >= 10) && (distance3 >= 10) && (distance4 >= 10)){
             printf("Distance Sense 1: %ld cm \n", distance1);
         nrf_delay_ms(50);
     }
-    else if (distance2 < 10 && distance1 >= 10 && distance3 >= 10)
+    else if (distance2 < 10 && distance1 >= 10 && distance3 >= 10 && distance4 >= 10)
     {
         printf("Distance Sense 2: %ld cm \n", distance2);
-        nrf_delay_ms(50);
+        //nrf_delay_ms(50);
     }
-    else if (distance1 < 10 && distance2 < 10 && distance3 >= 10){ 
-        printf("Distance Sense 1 and 2 together \n");
-        nrf_delay_ms(50);
-    }
-    else if (distance3 < 10 && distance1 >= 10 && distance2 >= 10){ 
+    // else if (distance1 < 10 && distance2 < 10 && distance3 >= 10){ 
+    //     printf("Distance Sense 1 and 2 together \n");
+    //     nrf_delay_ms(50);
+    // }
+    else if (distance3 < 10 && distance1 >= 10 && distance2 >= 10 && distance4 >= 10){ 
         printf("Distance Sense 3: %ld cm \n", distance3);
-        nrf_delay_ms(50);
+        //nrf_delay_ms(50);
     }
-    else if ((distance1 < 10) && (distance3 < 10) && (distance2 >= 10)){ 
-        printf("Distance Sense 1 and 3 together \n");
-        nrf_delay_ms(50);
+    else if (distance4 < 10 && distance1 >= 10 && distance2 >= 10 && distance3 >= 10){ 
+        printf("Distance Sense 4: %ld cm \n", distance4);
+        //nrf_delay_ms(50);
     }
-    else if ((distance2 < 10) && (distance3 < 10) && (distance1 >= 10)){ 
-        printf("Distance Sense 2 and 3 together \n");
-        nrf_delay_ms(50);
-    }
-    else if ((distance2 < 10) && (distance3 < 10) && (distance1 < 10)){ 
-        printf("Distance Sense 1 2 and 3 together \n");
-        nrf_delay_ms(50);
-    }
+    // else if ((distance1 < 10) && (distance3 < 10) && (distance2 >= 10)){ 
+    //     printf("Distance Sense 1 and 3 together \n");
+    //     nrf_delay_ms(50);
+    // }
+    // else if ((distance2 < 10) && (distance3 < 10) && (distance1 >= 10)){ 
+    //     printf("Distance Sense 2 and 3 together \n");
+    //     nrf_delay_ms(50);
+    // }
+    // else if ((distance2 < 10) && (distance3 < 10) && (distance1 < 10)){ 
+    //     printf("Distance Sense 1 2 and 3 together \n");
+    //     nrf_delay_ms(50);
+    // }
     else {
         nrf_delay_ms(50);
     }
-    */
-    play_tone(tones1, 3, 10, distance1, 50);
+    play_tone(tones1, 3, 15, distance1, 50);
+    play_tone(tones1, 3, 15, distance2, 50);
+    play_tone(tones1, 3, 15, distance3, 50);
+    play_tone(tones1, 3, 15, distance4, 50);
     nrf_delay_ms(50);
   }
 }
